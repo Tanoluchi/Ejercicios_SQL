4e511cb3fd17bd01975a42e7d61f30eba24d07dd
tanoluchi <lucianox15867@gmail.com>
2021-12-06 11:52:22 -0300

    first commit

---
+++ ejercicio2.sql
@@ -0,0 +1,6 @@
+/*
+Consulta la tabla payment de la base de datos sakila.
+¿Cuál es la cantidad mas baja y mas alta de la columna amount?
+*/
+
+SELECT DISTINCT amount FROM sakila.payment ORDER BY amount ASC;
---
+++ ejercicio3.sql
@@ -0,0 +1,7 @@
+/*
+Consulta description, release_year de la tabla film de la base de datos sakila.
+
+Filtra la información donde title sea IMPACT ALADDIN
+*/
+
+SELECT description, release_year FROM sakila.film WHERE title = 'IMPACT ALADDIN';
